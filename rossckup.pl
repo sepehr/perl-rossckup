d782a56d028dec647f74ca7adeff8d195ae58fa7
Sepehr Lajevardi <me@sepehr.ws>
2011-04-04 20:44:01 +0430

    Added inline license.

--- rossckup.pl
+++ rossckup.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/perl -w
+#
+# Rossckup.pl - http://github.com/sepehr/rossckup
+# Local and cross server backup for wash machines!
+#
+# Copyright (C) 2010, by Sepehr Lajevardi <me@sepehr.ws>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
 use warnings;
@@ -81,7 +98,7 @@ if (!BACKUP_FILES && !BACKUP_DB) {
   die "Nothing, Good!\n";
 }
 
-# Globals:
+# Backup date:
 my ($s, $m, $h, $day, $month, $year, $wd, $yd, $o) = localtime();
 $year += 1900;
 

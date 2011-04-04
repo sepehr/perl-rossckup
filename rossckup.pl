a68ca3e249ac7ecbe132ad822c718b54cfe757a5
Sepehr Lajevardi <me@sepehr.ws>
2011-04-04 20:01:44 +0430

    Dropped MIME::Lite::TT::HTML, Added SMTP support, Added strict pragma, etc.

--- rossckup.pl
+++ rossckup.pl
@@ -1,21 +1,27 @@
 #!/usr/bin/perl -w
 
+use strict;
+use warnings;
 use Net::FTP;
-use MIME::Lite::TT::HTML;
+use Net::SMTP::SSL;
 
 # Configs
 # @see http://perldoc.perl.org/constant.html
 use constant {
+  # System path to gunzip executable:
+  SYS_GZIP => '/bin/gzip',
+  # System path to dbdump executable:
+  SYS_DUMP => '/usr/bin/mysqldump',
   # Archive name of the backedup files:
   BACKUP_ARCHIVE_NAME => 'rossckup',
-  # Delete local archives after FTP upload:
-  BACKUP_DELETE_AFTER => 1,
+  # Local backup directory, no trailing slash:
+  BACKUP_DST => '/home/user/private_pool',
+  # First remove all files in BACKUP_DST:
+  BACKUP_RM => 1,
   # Local directory to backup, no trailing slash:
   BACKUP_SRC => '/home/user/public_html',
-  # Local backup directory, no trailing slash:
-  BACKUP_DST => '/home/user/backups',
-  # First remove all files in BACKUP_DST:
-  BACKUP_RM => 0,
+  # Whether to backup files:
+  BACKUP_FILES => 1,
   # Dump and backup database:
   BACKUP_DB => 1,
   # Database hostname:
@@ -25,30 +31,8 @@ use constant {
   # Database password:
   DB_PWD => '',
   # Space separated list of database names
-  # to backup, you can specify all to back 'em all:
+  # to backup, you can specify "all" to back 'em all:
   DB_NAMES => 'user_db1 user_db2 user_etc',
-  # Email after successful backup:
-  EMAIL_NOTIFY => 1,
-  # Also attach backup tarball to that email:
-  EMAIL_ATTACH => 1,
-  # Email sender:
-  EMAIL_FROM => 'rossckup@example.com',
-  # Email recipient:
-  EMAIL_TO => 'client@example.com',
-  # Email subject:
-  EMAIL_SUBJECT => 'Rossckup Notification',
-  # Email text template name:
-  EMAIL_TMPL_TXT => 'rossckup.txt.tt',
-  # Email HTML template name:
-  EMAIL_TMPL_HTML => 'rossckup.html.tt',
-  # Email templates directory path, no trailing slash:
-  EMAIL_TMPL_PATH => '/path/to/templates',
-  # Email charset:
-  EMAIL_CHARSET => 'utf8',
-  # Email encoding:
-  EMAIL_ENCODING => 'quoted-printable',
-  # Email timezone:
-  EMAIL_TIMEZONE => 'Asia/Tehran',
   # FTP upload after local backup:
   FTP_UPLOAD => 1,
   # FTP hostname:
@@ -59,58 +43,83 @@ use constant {
   FTP_PWD => '',
   # Remote server backup directory, no trailing slash:
   FTP_DST => '/',
-  # System path to gunzip executable:
-  SYS_GZIP => '/bin/gzip',
-  # System path to dbdump executable:
-  SYS_DUMP => '/usr/bin/mysqldump',
+  # Delete local archives after FTP upload:
+  FTP_DELETE_AFTER => 0,
+  # Email after successful backup:
+  EMAIL_NOTIFY => 1,
+  # SMTP server:
+  EMAIL_SMTP_HOST => 'smtp.example.com',
+  # SMTP username:
+  EMAIL_SMTP_USER => 'user+example.com',
+  # SMTP password:
+  EMAIL_SMTP_PWD => '',
+  # Email sender:
+  EMAIL_FROM => 'rossckup@example.com',
+  # Email recipient:
+  EMAIL_TO => 'fatass.boss@example.com',
+  # Email recipient name:
+  EMAIL_TO_NAME => 'Mr. Jonney Cowboy',
+  # Email subject:
+  EMAIL_SUBJECT => 'Rossckup Backup Notification',
+  # Email text body, available tokens:
+  # _to_name_
+  # _ftp_dst_
+  # _db_names_
+  # _ftp_host_
+  # _backup_src_
+  # _backup_dst_
+  EMAIL_BODY => "Dear _to_name_,\nBackup operation has been successfully done on your machine.",
+  # Email sgnature:
+  EMAIL_SIGNATURE => "\n\nAutomated Backup for Wash Machines!\nhttp://github.com/sepehr/rossckup",
 };
 
-# Email template params:
-my %params;
-$params{name} = 'Mr. Foo Bar';
+##### Warning: Donn touch me again! #####
 
-##### Warning: Donn touch me anymore! #####
+# Check configs:
+if (!BACKUP_FILES && !BACKUP_DB) {
+  # Exit:
+  die "Nothing, Good!\n";
+}
 
-# Set date:
-# @see http://perldoc.perl.org/functions/localtime.html
+# Globals:
 my ($s, $m, $h, $day, $month, $year, $wd, $yd, $o) = localtime();
 $year += 1900;
-# Set filenames:
-$dbckup = BACKUP_DST . '/dbs.tmp.sql.gz';
-$fileckup = BACKUP_DST . '/files.tmp.tar.gz';
 
-# Make local backup directory, if not exists:
+# Check local backup directory:
 if (! -d BACKUP_DST) {
   # Status:
   print('STATUS: Making backup directory ' . BACKUP_DST . "\n");
   # Issue:
   mkdir(BACKUP_DST, 0755)
-    or die "ERROR: There were a problem creating the local backup directory: $!";
+    or die "ERROR: There were a problem creating the local backup directory.\n";
 }
 
 # Remove old backups, if requested so:
 if (BACKUP_RM) {
-  $cmd = 'rm -rf ' . BACKUP_DST . '/*';
+  my $cmd = 'rm -rf ' . BACKUP_DST . '/*';
   # Status:
   print("STATUS: Executing $cmd\n");
   # Issue:
   system($cmd) == 0
-    or warn 'WARNING: There were a problem cleaning the backup destination directory.';
+    or warn "WARNING: There were a problem cleaning the backup destination directory.\n";
 }
 
-
 # Make the files tarball:
-$cmd = 'tar --exclude=' . BACKUP_DST . "/* -czPf $fileckup " . BACKUP_SRC;
-# Status:
-print("STATUS: Executing $cmd\n");
-# Issue:
-system($cmd) == 0
-  or warn 'WARNING: There were problems making files tarball.';
+my $fileckup = BACKUP_DST . '/' . BACKUP_ARCHIVE_NAME . ".files-$year$month$day.tar.gz";
+if (BACKUP_FILES) {
+  my $cmd = 'tar --exclude=' . BACKUP_DST . "/* -czPf $fileckup " . BACKUP_SRC;
+  # Status:
+  print("STATUS: Executing $cmd\n");
+  # Issue:
+  system($cmd) == 0
+    or warn 'WARNING: There were problems making files tarball.';
+}
 
 # Make databases tarball:
+my $dbckup = BACKUP_DST . '/' . BACKUP_ARCHIVE_NAME . ".dbs-$year$month$day.sql.gz";
 if (BACKUP_DB) {
   # Determine the proper command to issue:
-  $cmd = (DB_NAMES eq 'all') ?
+  my $cmd = (DB_NAMES eq 'all') ?
     SYS_DUMP . ' --host=' . DB_HOST . ' --user=' . DB_USER . ' --password=' . DB_PWD . " --add-drop-table --all-databases -c -l --result-file=$dbckup" :
     SYS_DUMP . ' --host=' . DB_HOST . ' --user=' . DB_USER . ' --password=' . DB_PWD . ' --add-drop-table --databases ' . DB_NAMES . ' -c -l | ' . SYS_GZIP . " > $dbckup";
   # Status:
@@ -121,56 +130,71 @@ if (BACKUP_DB) {
 }
 
 # Rearchive tarballs:
-$rossckup = BACKUP_DST . '/' . BACKUP_ARCHIVE_NAME . "-$year$month$day.tar.gz";
-$cmd = "tar -czPf $rossckup $dbckup $fileckup";
-# Status:
-print("STATUS: Executing $cmd\n");
-# Issue command:
-system($cmd) == 0
-  or die 'ERROR: Could not merge files and db tarballs.';
-
-# Delete previous tarballs:
-unlink $dbckup, $fileckup;
+my $rossckup = BACKUP_DST . '/' . BACKUP_ARCHIVE_NAME . "-$year$month$day.tar.gz";
+# Determine the proper command to issue:
+if (BACKUP_FILES && BACKUP_DB) {
+  my $cmd = "tar -czPf $rossckup $dbckup $fileckup";
+  # Status:
+  print("STATUS: Executing $cmd\n");
+  # Issue command:
+  system($cmd) == 0
+    or die "ERROR: Could not merge files and db tarballs.\n";
+  # Remove oldies:
+  unlink $fileckup, $dbckup;
+}
+# Files backup only:
+elsif (BACKUP_FILES) {
+  $rossckup = $fileckup;
+}
+# Databases backup only:
+else {
+  $rossckup = $dbckup;
+}
 
 # Email notification:
 # @see http://search.cpan.org/~chunzi/MIME-Lite-TT-HTML-0.03/lib/MIME/Lite/TT/HTML.pm
 if (EMAIL_NOTIFY) {
-  # Email template options:
-  my %options;
-  $options{INCLUDE_PATH} = EMAIL_TMPL_PATH;
+  # Status:
+  print('STATUS: Sending notification email to: ' . EMAIL_TO . "\n");
 
   # Set initials:
-  my $mail = MIME::Lite::TT::HTML->new(
-    To   => EMAIL_TO,
-    From => EMAIL_FROM,
-    Charset  => EMAIL_CHARSET,
-    Subject  => EMAIL_SUBJECT,
-    Timezone => EMAIL_TIMEZONE,
-    Encoding => EMAIL_ENCODING,
-    Template => {
-      text => EMAIL_TMPL_TXT,
-      html => EMAIL_TMPL_HTML,
-    },
-    TmplParams => \%params,
-    TmplOptions => \%options,
+  my $smtp = Net::SMTP::SSL->new(
+    EMAIL_SMTP_HOST,
+    Port => 465,
+    Debug => 0
+  ) or warn "WARNING: Could not connect to the specified SMTP server.\n";
+  # Authenticate:
+  $smtp->auth(EMAIL_SMTP_USER, EMAIL_SMTP_PWD)
+    or warn "WARNING: SMTP authention failed.";
+  # Set email addresses:
+  $smtp->mail(EMAIL_FROM);
+  $smtp->to(EMAIL_TO);
+  # Build the message:
+  $smtp->data;
+  # Dispatch, headers:
+  $smtp->datasend('From: ' . EMAIL_FROM . "\n");
+  $smtp->datasend('To: ' . EMAIL_TO . "\n");
+  $smtp->datasend('Subject: ' . EMAIL_SUBJECT . "\n");
+  $smtp->datasend("\n");
+  # Prepare bo"\nRossckup Automated Backups\nhttp://github.com/sepehr/rssckup"dy tokens:
+  # Replace tokens, PHP's strtr() equivalent somehow:
+  my %dic = (
+    '_to_name_' => EMAIL_TO_NAME,
+    '_ftp_dst_' => FTP_DST,
+    '_db_names_' => DB_NAMES,
+    '_ftp_host_' => FTP_HOST,
+    '_backup_src_' => BACKUP_SRC,
+    '_backup_dst_' => BACKUP_DST,
   );
-  # Attach the backup tarball:
-  if (EMAIL_ATTACH) {
-    # Set content type:
-    $mail->attr('content-type' => 'multipart/mixed');
-    # Attach the tarball:
-    $mail->attach(
-      Path => $rossckup,
-      Type => 'application/x-gzip',
-      Filename => BACKUP_ARCHIVE_NAME . "-$year$month$day.tar.gz",
-      Disposition => 'attachment',
-    );
-  }
+  my $tokens = join '|', keys %dic;
+  my $body = EMAIL_BODY;
+  $body =~ s/($tokens)/$dic{$1}/g;
+  # And send:
+  $smtp->datasend($body . EMAIL_SIGNATURE. "\n");
 
-  # Status:
-  print('STATUS: Sending notification email to ' . EMAIL_TO . "\n");
-  # Dispatch:
-  $mail->send();
+  # Closing SMTP session:
+  $smtp->dataend;
+  $smtp->quit;
 }
 
 # FTP upload:
@@ -180,27 +204,30 @@ if (FTP_UPLOAD) {
   print('STATUS: Uploading backup tarball via FTP: ' . FTP_HOST . "\n");
   # Connect:
   my $ftp = Net::FTP->new(FTP_HOST, Debug => 0)
-    or die 'ERROR: Could not connect to FTP host.';
+    or die "ERROR: Could not connect to FTP host.\n";
   # Login:
   $ftp->login(FTP_USER, FTP_PWD)
-    or die 'ERROR: Could not login to the FTP server.';
+    or die "ERROR: Could not login to the FTP server.\n";
   # Change dir:
   $ftp->cwd(FTP_DST)
-    or die 'ERROR: Could not change the FTP working directory.';
+    or die "ERROR: Could not change the FTP working directory.\n";
   # Set to binary mode:
   $ftp->binary();
   # Upload backups:
   $ftp->put($rossckup)
-    or die('ERROR: Could not upload the backups to the FTP server: ' . $ftp->message);
+    or die('ERROR: Could not upload the backups to the FTP server: ' . $ftp->message . "\n");
   # Close FTP session:
   $ftp->quit();
 
   # Delete local tarball:
-  if (BACKUP_DELETE_AFTER) {
+  if (FTP_DELETE_AFTER) {
     # Status:
     print("STATUS: Deleting local backup tarball.\n");
     # Delete:
     unlink $rossckup;
   }
 }
+
+# Be a good gal, say bye:
+print("Bye!\n");
 
